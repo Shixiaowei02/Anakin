047da7510f5a92fb145377cafeb069d037dbd746
shixiaowei02 <shixiaowei02@baidu.com>
2018-12-15 21:40:22 +0800

    delete other files

--- docs/run.py
+++
@@ -1,4 +0,0 @@
-from api_on_web import Doc as DocOnWeb
-
-if __name__ == "__main__":
-    DocOnWeb.run(host='0.0.0.0', port=8888, debug=True) 
--- framework/core/singleton.cpp
+++
@@ -1,8 +0,0 @@
-#include "framework/core/thread_safe_macros.h"
-
-namespace anakin {
-
-//! define the exit function.
-void default_exit() {}
-
-} /* namespace anakin */
